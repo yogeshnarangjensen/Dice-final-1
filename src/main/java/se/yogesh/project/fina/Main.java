2670cd7fe95f34622b529fbf2792762b91e84c1a
yogeshnarangjensen <yogeshnarangindia@gmail.com>
2024-10-02 12:07:06 +0200

    main method

---
+++ src/main/java/se/yogesh/project/fina/Main.java
@@ -0,0 +1,8 @@
+package se.yogesh.project.fina;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();  // Start the game
+    }
+}
